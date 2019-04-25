73974564da2e0a11173b6ddfb02c282e8bf0d80b
barbaradecares <barbaradecares@hotmail.com>
2019-04-24 18:20:14 -0800

    original code up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,30 @@
-import React from "react";
-import PropTypes from "prop-types";
-import logo from "../logo.svg";
+import React from 'react'
+import PropTypes from 'prop-types'
+import logo from '../logo.svg';
 
-const Navbar = ({ history, searchString }) => {
-  if (searchString.includes("recipe/")) {
-    searchString = "";
-  }
-  return (
+const Navbar = ({
+    searchString
+}) => (
     <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
-      <div className="navbar-brand col-1">
-        <img src={logo} className="Navbar-logo" alt="logo" />
-      </div>
+        <div className="navbar-brand col-1">
+            <img src={logo} className="Navbar-logo" alt="logo" />
+        </div>
 
-      <div className="form-group justify-content-center row col-10 my-2">
-        <input
-          value={searchString}
-          onChange={e => history.push(`/${e.target.value}`)}
-          className="form-control col-9 mr-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-      </div>
+        <div className="form-group justify-content-center row col-10 my-2">
+            <input
+                value={searchString}
+                // TODO: onChange deve atualizar a URL 
+                className="form-control col-9 mr-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+            />
+        </div>
     </nav>
-  );
-};
+)
 
 Navbar.propTypes = {
-  searchString: PropTypes.string
-};
+    searchString: PropTypes.string
+}
 
 export default Navbar;
